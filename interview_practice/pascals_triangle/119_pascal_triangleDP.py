22684db68269f09526baf5529cacfe8085b92b2b
vsedov <viv.sedov@hotmail.com>
2021-10-21 21:44:42 +0100

    Do basic ones first

---
+++ interview_practice/pascals_triangle/119_pascal_triangleDP.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © Viv Sedov
+#
+# File Name: 118_pascal_triangleDP.py
+__author__ = "Viv Sedov"
+__email__ = "viv.sv@hotmail.com"
+
+from typing import List
+
+import pyinspect as pi
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:  # noqa:ignore
+        ...
+
+
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    pi.install_traceback()
+    main()
